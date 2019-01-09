4dd5832eb20af2d3c6de148f855b43a4538c1c14
Rafael Henrique <rafael@formahomolog.com.br>
2019-01-09 14:45:49 -0200

    Config BD

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 //create a connection
 var connection = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	password:'root',
-	database:'login'
+	host:'us-cdbr-iron-east-01.cleardb.net',
+	user:'b7a1699b0ba87e',
+	password:'40a63db8',
+	database:'heroku_97cf472f3f1b796'
 });
 
 
