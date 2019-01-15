6f7e2cb87254491e39a90664b526bd6098e7071d
Rafael Henrique <rafael@formahomolog.com.br>
2019-01-15 12:09:52 -0200

    Configuran do o acesso ao BD

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 var mysql = require('mysql');
+let urlBd = process.env.CLEARDB_DATABASE_URL;
+let user = urlBd.substring(8,22);
+let password = urlBd.substring(23,31);
 //create a connection
 var connection = mysql.createConnection({
 	host:'us-cdbr-iron-east-01.cleardb.net',
-	user:'b7a1699b0ba87e',
-	password:'40a63db8',
+	user: user,
+	password: password,
 	database:'heroku_97cf472f3f1b796'
 });
 
